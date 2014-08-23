e6868455d2e14e1df0b70ca41ac68ac5ebb90f31
Jakub Kubacki <kubacki.jk@gmail.com>
2014-08-23 12:50:11 +0200

    Add name length validation to campaign.

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Campaign, :type => :model do
     c.name = ''
     expect(c).to_not be_valid
   end
+
+  it 'has name no shorter than 3 characters' do
+    c = FactoryGirl.build(:campaign)
+    c.name = 'Ca'
+    expect(c).to_not be_valid
+    expect(c).to_not be_valid
+    expect(c.errors[:name]).to include('is too short (minimum is 3 characters)')
+  end
 end
