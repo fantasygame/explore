6200b9f5e6671dfbd58873c7de8c6f3813b29e9e
Jakub Kubacki <kubacki.jk@gmail.com>
2014-08-23 12:45:43 +0200

    Fix typo in test

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Campaign, :type => :model do
   it 'has name' do
-    c = FactoryGirl.buid(:campaign)
+    c = FactoryGirl.build(:campaign)
     c.name = ''
     expect(c).to_not be_valid
   end
