fe03b6606e4ac3ed266d9cf1587890890b78876b
Małgorzata Grobelska <grobelska.m@gmail.com>
2014-08-23 13:22:42 +0200

    Create campaign model.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,6 @@
 class Campaign < ActiveRecord::Base
   has_many :characters
+  has_many :users, through: :characters
   has_many :events
   validates_presence_of :name
   validates_length_of :name, minimum: 3
