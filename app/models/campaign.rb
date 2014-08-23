5ff88f2c8415547f29f2f09f138abc127baeba9c
Małgorzata Grobelska <grobelska.m@gmail.com>
2014-08-23 15:54:00 +0200

    Add level method to character.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,5 +4,5 @@ class Campaign < ActiveRecord::Base
   has_many :events
   validates_presence_of :name
   validates_length_of :name, minimum: 3
-  validates_presence_of :characters
+  # validates_presence_of :characters
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,29 @@
 class Character < ActiveRecord::Base
   belongs_to :user
   belongs_to :campaign
+  validates_presence_of :exp
+  validates_numericality_of :exp, greater_than_or_equal_to: 0
+  def level
+    if self.exp < 0
+      def raise_exception
+      puts 'Exp cannot be lower than 0 you stupid motherfucker!'
+      throw 'An error has occured.'
+      puts 'I am raising you bitch.'
+      end
+      raise_exception
+    else current_exp = self.exp / 1000
+      lvl = 1
+
+      while current_exp >= lvl do
+        current_exp = current_exp - lvl
+        lvl += 1
+      end
+
+      lvl
+      end
+    end
+
+
+
+
 end
