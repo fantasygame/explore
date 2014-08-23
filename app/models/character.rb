624683b32f8f4f0ad2b83885ab629f10aa887c26
Jakub Kubacki <kubacki.jk@gmail.com>
2014-08-23 18:30:10 +0200

    Refactor level method.

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,26 +3,15 @@ class Character < ActiveRecord::Base
   belongs_to :campaign
   validates_presence_of :exp
   validates_numericality_of :exp, greater_than_or_equal_to: 0
+
   def level
-    if self.exp < 0
-      def raise_exception
-      puts 'Exp cannot be lower than 0 you stupid motherfucker!'
-      throw 'An error has occured.'
-      puts 'I am raising you bitch.'
-      end
-      raise_exception
-    else current_exp = self.exp / 1000
-      lvl = 1
-
-      while current_exp >= lvl do
-        current_exp = current_exp - lvl
-        lvl += 1
-      end
-      lvl
-      end
+    raise 'Exp cannot be less than 0.' if self.exp < 0
+    current_exp = self.exp / 1000
+    lvl = 1
+    while current_exp >= lvl do
+      current_exp = current_exp - lvl
+      lvl += 1
     end
-
-
-
-
+    lvl
+  end
 end
